b51de31f7bb43299a73addfeb4e82d2cc73d0c79
eromose-le <erons24@yahoo.com>
2022-05-24 01:52:23 +0100

    coin detailed and home page implemented

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import CoinDetailedScreen from './src/screens/CoinDetailedScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CoinDetailedScreen />
+      <HomeScreen />
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    paddingTop: 50
+  }
+});
